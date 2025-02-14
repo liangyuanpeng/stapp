40eded26b9321c38a46c97b0ec1ac1177caf5587
Lan Liang <gcslyp@gmail.com>
2025-02-14 08:52:22 +0000

    u

--- hack/test.sh
+++ hack/test.sh
@@ -2,6 +2,7 @@
 
 #TODO cleanup zot
 export OCI_REG_PLAIN_HTTP=on
+#TODO auto-update zot version
 export ZOT_VERSION=${ZOT_VERSION:-"2.1.0"}
 docker run -d -p 5000:5000 --name zot ghcr.io/project-zot/zot:v${ZOT_VERSION}
 sleep 1
